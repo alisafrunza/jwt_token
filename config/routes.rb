64ed135764481f84e8f3449ce15caa727d5088b6
alisafrunza <alisafrunza@gmail.com>
2018-03-06 17:32:18 +0200

    removed comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :personal_data, only: [:index] do
-      	post :authorize, on: :collection
+        post :authorize, on: :collection
       end
     end
   end
